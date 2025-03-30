52e06c5bafce42d251c6400d3a030ebdd0512a69
hawon100 <hawonoj002@gmail.com>
2025-03-30 18:03:20 +0900

    basic setting

---
+++ src/test/java/com/example/jdbcexam/JdbcexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdbcexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
